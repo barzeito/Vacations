e7bbe0d90666528b45c0bebd6277e431f3abe78f
barzeito <barzeitony@gmail.com>
2024-03-31 18:56:28 -0700

    Added Vacations + Fixed Home Page CSS

--- frontend/src/components/home/cards/Cards.tsx
+++ frontend/src/components/home/cards/Cards.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import VacationModel from "../../../models/VacationModel";
 import formatDate from "../../../utils/formatDate";
 import formatPrice from "../../../utils/formatPrice";
@@ -6,12 +7,24 @@ import "./Cards.css";
 interface vacationsCardsProps {
     vacation: VacationModel
 }
+
 function Cards(props: vacationsCardsProps): JSX.Element {
+
+    const [liked, setLiked] = useState(false);
+
+    const handleLike = () => {
+        setLiked(!liked);
+    };
+
     return (
         <div className="Cards">
             <div className="Card">
                 <div className="cardTop">
-                    <div className="cardFollow">follow</div>
+                    <label className={`cardFollow ${liked ? 'liked' : ''}`}>
+                        <input type="checkbox" checked={liked} onChange={handleLike} />
+                        {liked ? 'Liked' : 'Like'}
+                        <i className="fas fa-heart"></i>
+                    </label>
                     <img src={props.vacation.imageUrl} className="cardImage"></img>
                     <div className="cardName">{props.vacation.destination}</div>
                     <div className="cardDates">{props.vacation.startDate && formatDate(props.vacation.startDate)} - {props.vacation.endDate && formatDate(props.vacation.endDate)}</div>
